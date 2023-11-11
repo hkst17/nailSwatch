7fbc867cd447e4451ec5513c58c14def9a0244d1
Sandra Latt <slatt@ucsc.edu>
2023-11-10 17:34:29 -0800

    removed repetitive code and one row of buttons
    removed the other buttons for each nail.

--- nailSwatches/index.js
+++ nailSwatches/index.js
@@ -1,40 +1,14 @@
-/*
-console.log('script loaded...');
-
-function paint(img){
+var num = 1;
+function finger(img){
     console.log(img);
-    const nail = document.getElementById('nailID');
-    nail.style = `background-color:${img}`;
-
-}
-*/
-function nail0(img) {
-    console.log(img);
-    const nail = document.getElementById('finger1ID');
-    nail.style = `background-image: url(${img})`;
-}
-function nail1(img) {
-    console.log(img);
-    const nail = document.getElementById('finger2ID');
-    nail.style = `background-image: url(${img})`;
-}
-function nail2(img) {
-    console.log(img);
-    const nail = document.getElementById('finger3ID');
-    nail.style = `background-image: url(${img})`;
-}
-function nail3(img) {
-    console.log(img);
-    const nail = document.getElementById('finger4ID');
-    nail.style = `background-image: url(${img})`;
-}
-function nail4(img) {
-    console.log(img);
-    const nail = document.getElementById('finger5ID');
-    nail.style = `background-image: url(${img})`;
+    const finger = document.getElementById('finger'+num+'ID');
+    finger.style = `background-image: url(${img})`;
+    num++;
+    
 }
 
 function reset(color) {
+    num = 1;
     document.getElementById('finger1ID').style = `background-color:${color}`;
     document.getElementById('finger2ID').style = `background-color:${color}`;
     document.getElementById('finger3ID').style = `background-color:${color}`;
