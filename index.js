2cab08b738b6dc2bf54fee0e90a885489ff7ac0c
Sandra Latt <slatt@ucsc.edu>
2024-01-10 18:50:56 -0800

    Tried fixing the broken things to polish

--- index.js
+++ index.js
@@ -14,53 +14,58 @@ pickerInd.style.visibility = 'hidden';
 const indQ = document.getElementById('indQ');
 indQ.style.display = 'none';
 
-var modal = document.getElementById("myModal");
+// var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("dot");
+// // Get the button that opens the modal
+// var btn = document.getElementById("dot");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function () {
-    modal.style.display = "block";
-}
+// // When the user clicks the button, open the modal 
+// btn.onclick = function () {
+//     modal.style.display = "block";
+// }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
-}
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function () {
+//     modal.style.display = "none";
+// }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function (event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
 
 designOrColorText.addEventListener("click", function () {
     if (designOrColorText.textContent == "Design") {
-
-        designOrColorText.textContent = "Color";
-        designOrColorBool = false;
-        colorPicker1.style.display = "none";
-
+        color();
     }
     else {
-        designOrColorText.textContent = "Design";
-        // colorPicker1.style.visibility = 'hidden';
-        pickerInd.style.visibility = "hidden";
-        // colorPicker1.style.visibility = "hidden";
-        colorPicker1.style.display = "inline-flex";
-        designOrColorBool = true;
-        triangle.style.visibility = 'hidden';
+        design();
     }
 });
 
+function design() {
+    designOrColorText.textContent = "Design";
+    // colorPicker1.style.visibility = 'hidden';
+    pickerInd.style.visibility = "hidden";
+    // colorPicker1.style.visibility = "hidden";
+    colorPicker1.style.display = "inline-flex";
+    designOrColorBool = true;
+    triangle.style.visibility = 'hidden';
+}
+
+function color() {
+    designOrColorText.textContent = "Color";
+    designOrColorBool = false;
+    colorPicker1.style.display = "none";
+}
+
 function buttonAlert() {
     if (document.getElementById("designOrColor").disabled == true) {
-
         alert("Please select a style for this nail first");
     }
 }
@@ -106,12 +111,14 @@ function reset(color) { // reset all nails
     numDesign = 0;
     clear = 0;
     document.getElementById('price').style.display = 'none';
+    // colorPicker1.style.display = "none";
+    pickerInd.style.display = "none";
+    indQ.style.display = "none";
+    design();
+    document.getElementById('picker').style.display = 'none';
     triangle.style.visibility = 'visible';
     triangle.style.left = '404px';
     triangle.style.top = '225px';
-    colorPicker1.style.display = "none";
-    pickerInd.style.display = "none";
-    indQ.style.display = "none";
     for (let i = 1; i < 6; i++) {
         document.getElementById('finger' + i + 'ID').style = `background-image:  url('./img/emptyNail.png')`;
     }
@@ -242,16 +249,16 @@ function clickHandler(clicked_id) { // resetting specific nail after all designs
 }
 
 function alwaysRunning() {
-    if (clear > 0 && num > 5) { // if clear nail is in set & set is finished
-        // if (clear > 0) { // if clear nail is in set & set is finished
-        // colorPicker1.style.display = "inline-flex";
-    }
-    if (colorPicker1.style.display == "inline-flex") {   // based on if the color picker is shown, position triangle in the correct position
-        // triangle.style.top = '270px';
-    }
-    if (colorPicker1.style.display == "none") {  // same as above
-        // triangle.style.top = '225px';
-    }
+    // if (clear > 0 && num > 5) { // if clear nail is in set & set is finished
+    //     // if (clear > 0) { // if clear nail is in set & set is finished
+    //     // colorPicker1.style.display = "inline-flex";
+    // }
+    // if (colorPicker1.style.display == "inline-flex") {   // based on if the color picker is shown, position triangle in the correct position
+    //     // triangle.style.top = '270px';
+    // }
+    // if (colorPicker1.style.display == "none") {  // same as above
+    //     // triangle.style.top = '225px';
+    // }
 }
 
 function price(amount) {
@@ -264,7 +271,9 @@ function testCheck() {
     console.log("numDesign: " + numDesign);
     console.log("bkrd img: " + document.getElementById('finger1ID').style.backgroundImage);
     console.log("design or color text: " + designOrColorText.textContent);
+    console.log("design or color text: " + designOrColorBool);
     console.log("clear: " + clear);
+    console.log("is clear: " + isNailClear);
 }
 
 setInterval(alwaysRunning, 100);
